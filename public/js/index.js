1f2aea6b92d8a67f25c3617e1cbc3c26804e8cd0
Diogo Cardoso <diogocardoso92@outlook.com>
2016-11-24 17:11:23 +0000

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -26,11 +26,12 @@ socket.on('disconnect', () => {
 
 $('#message-form').submit((e) => {
     e.preventDefault();
+    const messageTextbox = $('[name=message]');
     socket.emit('createMessage', {
         from: 'User',
-        text: $('[name=message]').val()
+        text: messageTextbox.val()
     }, () => {
-
+        messageTextbox.val('');
     });
 });
 
@@ -39,12 +40,15 @@ locationButton.on('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser');
     }
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
     navigator.geolocation.getCurrentPosition((position) => {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,         
             longitude: position.coords.longitude
         });
     }, () => {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location');
     });
 });
