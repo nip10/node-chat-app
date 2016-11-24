a165138f782916451e5154c702a0f6f92a5ea7aa
nip10 <tuniq@live.com.pt>
2016-11-24 16:22:29 +0000

    Add form for messages and show incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -6,8 +6,22 @@ socket.on('connect', () => {
 
 socket.on('newMessage', (message) => {
     console.log('New message', message);
+    const li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    $('#messages').append(li);
 });
 
 socket.on('disconnect', () => {
     console.log('Disconnected from server');
+});
+
+$('#message-form').submit((e) => {
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: 'User',
+        text: $('[name=message]').val()
+    }, () => {
+
+    });
 });
