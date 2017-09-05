89acdb2e8ec22b2e57a1c0f0638b9be7bd73c434
Diogo Cardoso <diogocardoso92@outlook.com>
2017-09-05 15:56:38 +0100

    Deleted temp file

--- playground/time.js
+++
@@ -1,8 +0,0 @@
-const moment = require('moment');
-
-const someTimestamp = moment().valueOf();
-console.log(someTimestamp);
-
-const createdAt = 1234;
-const date = moment(createdAt);
-console.log(date.format('hh:mm a'));
